812638b1961dedf0f07d94de26d444bb030dabef
naocraft0111 <naoki1306@gmail.com>
2022-10-28 22:28:02 +0900

    メニュー画面の編集・削除

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -95,7 +95,9 @@ class ProductController extends Controller
      */
     public function edit($id)
     {
-        //
+        $product = Product::find($id);
+        $categories = Category::all();
+        return view('product.edit', compact('product','categories'));
     }
 
     /**
@@ -107,7 +109,37 @@ class ProductController extends Controller
      */
     public function update(Request $request, $id)
     {
-        //
+        request()->validate(
+            [
+                'name' => 'required',
+                'description' => 'required',
+                'price' => 'required|integer',
+                'category' => 'required',
+                'image' => 'mimes:jpeg,png,jpg,gif,svg'
+            ],
+            [
+                'name.required' => '商品名を入力してください。',
+                'description.required' => '詳細を入力してください。',
+                'price.required' => '値段を入力してください。',
+                'category.required' => 'カテゴリーを入力してください。'
+            ]
+        );
+        $product = Product::find($id);
+        $name = $product->image;
+        if ($request->hasFile('image')) {
+            $image = $request->file('image');
+            $name = time() . '.' . $image->getClientOriginalExtension();
+            $destinationPath = public_path('/images');
+            $image->move($destinationPath, $name);
+        }
+        $product->update([
+            'name' => request('name'),
+            'description' => request('description'),
+            'price' => request('price'),
+            'category_id' => request('category'),
+            'image' => $name
+        ]);
+        return to_route('product.index')->with('message', '商品情報が更新されました。');
     }
 
     /**
@@ -118,6 +150,8 @@ class ProductController extends Controller
      */
     public function destroy($id)
     {
-        //
+        $product = Product::find($id);
+        $product->delete();
+        return to_route('product.index')->with('message', '商品情報が削除されました。');
     }
 }
